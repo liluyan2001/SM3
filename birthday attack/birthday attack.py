0f068a6e7483f8d707ca040fd81cb0906373b55f
liluyan2001 <109586535+liluyan2001@users.noreply.github.com>
2022-07-20 09:46:48 +0800

    Update birthday attack.py

--- birthday attack/birthday attack.py
+++ birthday attack/birthday attack.py
@@ -20,14 +20,14 @@ def byte_to_str(byte):  # byte转字符串
     return str1.decode('utf-8')
 
 for j in range(0,1000):
-    for i in range(0,2**16):
-        x = str_to_byte(str(random.randint(0,2**256)))
+    for i in range(0,2**16):  
+        x = str_to_byte(str(random.randint(0,2**256))) #随机生成一个byte类型
         y = str_to_byte(str(random.randint(0,2**256)))
 
-        x_EN = sm3.sm3_hash(func.bytes_to_list(x))
+        x_EN = sm3.sm3_hash(func.bytes_to_list(x))# SM3加密
         y_EN = sm3.sm3_hash(func.bytes_to_list(y))
 
-        if(x_EN[0:8] ==y_EN[0:8]):
+        if(x_EN[0:8] ==y_EN[0:8]):#前8位进行比较
             print("True")
             print(x),print(y)
-    print("计数器:",j)
+    print("计数器:",j)  #计数器
